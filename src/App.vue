957b72d8f7636515669824e7de745538308f13ee
Willian Sato <willianmassayukisato@gmail.com>
2026-03-28 16:07:22 -0300

    feat: variable control via button

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
+  <h1>Basics</h1>
+  <h2>Passing props to children components</h2>
   <div class="container-box">
     <MyBox text="1" />
     <MyBox text="2" />
     <MyBox text="3" />
   </div>
+
+  <h2>Getting props from children components</h2>
+  <div>
+    <p>{{ count }}</p>
+    <MyButton @update="decrementCount" title="-1" />
+    <MyButton @update="incrementCount" title="+1" />
+    <MyButton @update="clear" title="Zerar" />
+  </div>
 </template>
 
 <script>
 import MyBox from "./components/MyBox.vue";
+import MyButton from "./components/MyButton.vue";
 
 export default {
   name: "App",
   components: {
     MyBox,
+    MyButton,
+  },
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  methods: {
+    incrementCount() {
+      this.count++;
+    },
+    decrementCount() {
+      this.count--;
+    },
+    clear() {
+      this.count = 0;
+    },
   },
 };
 </script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="$emit('update')">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+
+  props: {
+    title: {
+      type: String,
+      default: "+1",
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-family: inherit;
+  background-color: blueviolet;
+}
+</style>
